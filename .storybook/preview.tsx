28b97ab0206a0a16a438242a8a54f1f67a8170e7
Inge Fossland <inge@fosslandfoss.no>
2024-11-01 14:53:31 +0100

    chore: Add theming for all stories (#16)

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,9 +1,10 @@
 import "../lib/css/global.css";
 import "./preview.css";
-import {StoryFn} from "@storybook/react";
+import { withThemeByDataAttribute } from "@storybook/addon-themes";
+import { Preview, StoryFn } from "@storybook/react";
 
 /** @type { import('@storybook/react').Preview } */
-const preview = {
+const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
@@ -13,21 +14,33 @@ const preview = {
     },
   },
   decorators: [
-    (Story: StoryFn, { tags }: { tags: string[]}) => {
-    const isStable = (tags || []).includes('stable');
-    const state = isStable ? 'stable' : 'experimental';
-    return (
-        <>
-          <span className='preview-container-stage-tag' data-tag={state}>
+    (Story: StoryFn, data) => {
+      const { tags, globals } = data;
+
+      const isStable = (tags || []).includes("stable");
+      const state = isStable ? "stable" : "experimental";
+
+      console.log("Data", data);
+
+      return (
+        <div data-theme={globals?.theme}>
+          <span className="preview-container-stage-tag" data-tag={state}>
             {state}
           </span>
           <Story />
-        </>
-    );
+        </div>
+      );
     },
+    withThemeByDataAttribute({
+      themes: {
+        global: "global",
+        neutral: "neutral",
+        company: "company",
+        person: "person",
+      },
+      defaultTheme: "neutral",
+    }),
   ],
 };
-
-
 
 export default preview;
