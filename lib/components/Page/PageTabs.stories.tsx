04685ac0ff8b7c79751327d0e24ce15cd3aa0559
Inge Fossland <inge@fosslandfoss.no>
2025-02-27 15:06:23 +0100

    fix: Page tabs selected

---
+++ lib/components/Page/PageTabs.stories.tsx
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { PageTabs } from '../';
+
+const meta = {
+  title: 'Page/PageTabs',
+  component: PageTabs,
+  tags: ['autodocs', 'beta'],
+  parameters: {},
+  args: {
+    items: [
+      {
+        id: '1',
+        href: '#1',
+        title: 'Tab 1',
+        selected: true,
+      },
+      {
+        id: '2',
+        href: '#2',
+        title: 'Tab 2',
+      },
+      {
+        id: '3',
+        href: '#3',
+        title: 'Tab 3',
+      },
+    ],
+  },
+} satisfies Meta<typeof PageTabs>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {},
+};
