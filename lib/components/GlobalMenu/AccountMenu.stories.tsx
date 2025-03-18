c39ee8d7660fb547d4fc6801496034c591b7d475
Sean Erik Scully <sean.erik.scully@gmail.com>
2025-03-18 14:10:26 +0100

    draft: sub items for accounts menu

--- lib/components/GlobalMenu/AccountMenu.stories.tsx
+++ lib/components/GlobalMenu/AccountMenu.stories.tsx
@@ -1,6 +1,121 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { accountMenu } from '../../../examples';
-import { AccountMenu } from './AccountMenu';
+import { AccountMenu, type AccountMenuProps } from './AccountMenu';
+
+const accountMenuWithSubItems: AccountMenuProps = {
+  accountSearch: {
+    placeholder: 'finn',
+    name: 'search-account',
+    getResultsLabel: (hits = 0) => {
+      if (hits) {
+        return hits + ' treff';
+      }
+      return 'Ingen treff';
+    },
+    hidden: false,
+  },
+  accountGroups: {
+    primary: {
+      title: 'Deg selv og favoritter',
+    },
+    secondary: {
+      title: 'Andre kontoer',
+    },
+  },
+  accounts: [
+    {
+      id: 'party:mathias',
+      groupId: 'primary',
+      type: 'person',
+      name: 'Mathias Dyngeland',
+      badge: {
+        label: '12',
+      },
+      alertBadge: {
+        label: '7 uleste',
+      },
+    },
+    {
+      id: 'party:bergerbar',
+      groupId: 'favourites',
+      type: 'company',
+      name: 'Bergen bar',
+      badge: {
+        label: '2',
+      },
+      items: [
+        {
+          id: 'party:bergerbar-sub2',
+          type: 'company',
+          name: 'Bergen SubParty 1',
+          badge: {
+            label: '2',
+          },
+        },
+        {
+          id: 'party:bergerbar-sub3 ',
+          type: 'company',
+          name: 'Bergen SubParty 2',
+          badge: {
+            label: '2',
+          },
+        },
+      ],
+    },
+    {
+      id: 'party:brann',
+      groupId: 'favourites',
+      type: 'company',
+      name: 'Sportsklubben Brann',
+      badge: {
+        label: '34',
+      },
+    },
+    {
+      id: 'allAccounts',
+      groupId: 'groups',
+      type: 'company',
+      name: 'Alle virksomheter',
+      accountNames: ['Keeperhansker AS', 'Stadion drift AS', 'Landslaget'],
+      selected: false,
+      badge: {
+        size: 'xs',
+        label: '19',
+      },
+    },
+    {
+      id: 'party:keeperhansker',
+      groupId: 'secondary',
+      type: 'company',
+      name: 'Keeperhansker AS',
+      badge: {
+        label: '34',
+      },
+      alertBadge: {
+        label: '2 uleste',
+      },
+    },
+    {
+      id: 'party:stadiondrift',
+      groupId: 'secondary',
+      type: 'company',
+      name: 'Stadion drift AS',
+      badge: {
+        label: '34',
+      },
+    },
+    {
+      id: 'party:landslaget',
+      groupId: 'secondary',
+      type: 'company',
+      name: 'Landslaget',
+      badge: {
+        size: 'xs',
+        label: '34',
+      },
+    },
+  ],
+};
 
 const meta = {
   title: 'Menu/AccountMenu',
@@ -17,4 +132,10 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {},
+};
+
+export const SubItems: Story = {
+  args: {
+    ...accountMenuWithSubItems,
+  },
 };
