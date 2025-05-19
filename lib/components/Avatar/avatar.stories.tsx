a9b5e3c2500558f0501f62d63471ca1e9cb8d4ae
Sean Erik Scully <sean.erik.scully@gmail.com>
2025-05-19 13:25:31 +0200

    chore: add logo url comparisons

--- lib/components/Avatar/avatar.stories.tsx
+++ lib/components/Avatar/avatar.stories.tsx
@@ -1,5 +1,8 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import orgs from '../../../examples/logos/orgs.json'
+import orgs2 from '../../../examples/logos/orgs2.json'
 import { Avatar } from './';
+import {Flex} from "../Page";
 
 const meta = {
   title: 'Avatar/Avatar',
@@ -40,3 +43,212 @@ export const Logo: Story = {
     size: 'xl',
   },
 };
+
+
+export const LogoComparison = () => {
+  return (
+      <Flex spacing={2} direction="col" padding={2}>
+        <Flex direction="row" spacing={2}>
+          {Object.keys(orgs2.orgs).slice(0, 10).map((org) => {
+            const orgFound = orgs.find((o) => org === o.id);
+            return (
+                <Flex spacing={2} direction="col">
+                  <p>Altinn CDN</p>
+                  <Avatar
+                      key={org}
+                      type="company"
+                      name={org}
+                      imageUrl={orgs2.orgs[org]?.logo}
+                      size="md"
+                  />
+                  <p>OrgLogo</p>
+                  <Avatar
+                      key={org + '-2'}
+                      type="company"
+                      name={org}
+                      imageUrl={orgFound?.logo}
+                      size="md"
+                  />
+                </Flex>
+            )
+          })}
+        </Flex>
+        <Flex direction="row" spacing={2}>
+          {Object.keys(orgs2.orgs).slice(10, 20).map((org) => {
+            const orgFound = orgs.find((o) => org === o.id);
+            return (
+                <Flex spacing={2} direction="col">
+                  <p>Altinn CDN</p>
+                  <Avatar
+                      key={org}
+                      type="company"
+                      name={org}
+                      imageUrl={orgs2.orgs[org]?.logo}
+                      size="md"
+                  />
+                  <p>OrgLogo</p>
+                  <Avatar
+                      key={org + '-2'}
+                      type="company"
+                      name={org}
+                      imageUrl={orgFound?.logo}
+                      size="md"
+                  />
+                </Flex>
+            )
+          })}
+        </Flex>
+        <Flex direction="row" spacing={2}>
+          {Object.keys(orgs2.orgs).slice(20, 30).map((org) => {
+            const orgFound = orgs.find((o) => org === o.id);
+            return (
+                <Flex spacing={2} direction="col">
+                  <p>Altinn CDN</p>
+                  <Avatar
+                      key={org}
+                      type="company"
+                      name={org}
+                      imageUrl={orgs2.orgs[org]?.logo}
+                      size="md"
+                  />
+                  <p>OrgLogo</p>
+                  <Avatar
+                      key={org + '-2'}
+                      type="company"
+                      name={org}
+                      imageUrl={orgFound?.logo}
+                      size="md"
+                  />
+                </Flex>
+            )
+          })}
+        </Flex>
+        <Flex direction="row" spacing={2}>
+          {Object.keys(orgs2.orgs).slice(30, 40).map((org) => {
+            const orgFound = orgs.find((o) => org === o.id);
+            return (
+                <Flex spacing={2} direction="col">
+                  <p>Altinn CDN</p>
+                  <Avatar
+                      key={org}
+                      type="company"
+                      name={org}
+                      imageUrl={orgs2.orgs[org]?.logo}
+                      size="md"
+                  />
+                  <p>OrgLogo</p>
+                  <Avatar
+                      key={org + '-2'}
+                      type="company"
+                      name={org}
+                      imageUrl={orgFound?.logo}
+                      size="md"
+                  />
+                </Flex>
+            )
+          })}
+        </Flex>
+        <Flex direction="row" spacing={2}>
+          {Object.keys(orgs2.orgs).slice(40, 50).map((org) => {
+            const orgFound = orgs.find((o) => org === o.id);
+            return (
+                <Flex spacing={2} direction="col">
+                  <p>Altinn CDN</p>
+                  <Avatar
+                      key={org}
+                      type="company"
+                      name={org}
+                      imageUrl={orgs2.orgs[org]?.logo}
+                      size="md"
+                  />
+                  <p>OrgLogo</p>
+                  <Avatar
+                      key={org + '-2'}
+                      type="company"
+                      name={org}
+                      imageUrl={orgFound?.logo}
+                      size="md"
+                  />
+                </Flex>
+            )
+          })}
+        </Flex>
+        <Flex direction="row" spacing={2}>
+          {Object.keys(orgs2.orgs).slice(50, 60).map((org) => {
+            const orgFound = orgs.find((o) => org === o.id);
+            return (
+                <Flex spacing={2} direction="col">
+                  <p>Altinn CDN</p>
+                  <Avatar
+                      key={org}
+                      type="company"
+                      name={org}
+                      imageUrl={orgs2.orgs[org]?.logo}
+                      size="md"
+                  />
+                  <p>OrgLogo</p>
+                  <Avatar
+                      key={org + '-2'}
+                      type="company"
+                      name={org}
+                      imageUrl={orgFound?.logo}
+                      size="md"
+                  />
+                </Flex>
+            )
+          })}
+        </Flex>
+        <Flex direction="row" spacing={2}>
+          {Object.keys(orgs2.orgs).slice(60, 70).map((org) => {
+            const orgFound = orgs.find((o) => org === o.id);
+            return (
+                <Flex spacing={2} direction="col">
+                  <p>Altinn CDN</p>
+                  <Avatar
+                      key={org}
+                      type="company"
+                      name={org}
+                      imageUrl={orgs2.orgs[org]?.logo}
+                      size="md"
+                  />
+                  <p>OrgLogo</p>
+                  <Avatar
+                      key={org + '-2'}
+                      type="company"
+                      name={org}
+                      imageUrl={orgFound?.logo}
+                      size="md"
+                  />
+                </Flex>
+            )
+          })}
+        </Flex>
+        <Flex direction="row" spacing={2}>
+          {Object.keys(orgs2.orgs).slice(70).map((org) => {
+            const orgFound = orgs.find((o) => org === o.id);
+            return (
+                <Flex spacing={2} direction="col">
+                  <p>Altinn CDN</p>
+                  <Avatar
+                      key={org}
+                      type="company"
+                      name={org}
+                      imageUrl={orgs2.orgs[org]?.logo}
+                      size="md"
+                  />
+                  <p>OrgLogo</p>
+                  <Avatar
+                      key={org + '-2'}
+                      type="company"
+                      name={org}
+                      imageUrl={orgFound?.logo}
+                      size="md"
+                  />
+                </Flex>
+            )
+          })}
+        </Flex>
+      </Flex>
+  )
+}
+
