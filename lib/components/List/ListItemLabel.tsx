699c2a8833d90ac44a24a2d5e5ac500132d4305c
Vedeler <acn-avede@ai-dev.no>
2025-07-15 16:15:57 +0200

    remove console logs

--- lib/components/List/ListItemLabel.tsx
+++ lib/components/List/ListItemLabel.tsx
@@ -83,7 +83,6 @@ export const ListItemLabel = ({
   if (title && value) {
     const titleProps = title && getTitleProps(title);
     const descriptionProps = value && getDescriptionProps(value);
-    console.log('value', value); // Debugging line, can be removed later
 
     return (
       <span className={cx(styles.label, className)} id={id} data-size={size}>
@@ -102,8 +101,6 @@ export const ListItemLabel = ({
   const titleProps = title && getTitleProps(title);
   const descriptionProps = description && getDescriptionProps(description);
 
-  console.log('titleProps', titleProps); // Debugging line, can be removed later
-  console.log('descriptionProps', descriptionProps); // Debugging line, can be removed later
   return (
     <span className={cx(styles.label, className)} id={id} data-size={size}>
       {children ? (
