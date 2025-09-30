8fa34ea82fc5c583e0e27369053e4d05712fd545
Mateusz Marek Bacherycz <mbacherycz@icloud.com>
2025-09-30 15:45:31 +0200

    chore: Adjust Alert styles (#679)

--- lib/components/Alert/Alert.tsx
+++ lib/components/Alert/Alert.tsx
@@ -1,6 +1,7 @@
 import { Alert as DsAlert } from '@digdir/designsystemet-react';
 import type { ReactNode } from 'react';
 import { Heading, Typography } from '../Typography';
+import styles from './alert.module.css';
 
 export type AlertVariant = 'info' | 'success' | 'warning' | 'danger';
 
@@ -14,8 +15,8 @@ export interface AlertProps {
 export const Alert = ({ variant, heading, message, children }: AlertProps) => {
   return (
     <DsAlert data-color={variant}>
-      <Heading>{heading}</Heading>
-      {message && <Typography>{message}</Typography>}
+      <Heading className={styles.heading}>{heading}</Heading>
+      {message && <Typography className={styles.message}>{message}</Typography>}
       {children}
     </DsAlert>
   );
