c3332cad74aea2eddee0cb0d2f70e92de6314ae4
Alexander Midteide <alexander.midteide@digdir.no>
2025-10-09 15:05:51 +0200

    feat: Option for betaBadge on Altinn logo in header

--- lib/components/Header/HeaderLogo.tsx
+++ lib/components/Header/HeaderLogo.tsx
@@ -1,5 +1,6 @@
 import cx from 'classnames';
 import type { ElementType, MouseEventHandler } from 'react';
+import { Badge } from '../Badge/Badge.tsx';
 import { ButtonBase } from '../Button';
 import { DigdirLogomark } from './DigdirLogomark.tsx';
 import styles from './headerLogo.module.css';
@@ -10,13 +11,14 @@ export interface HeaderLogoProps {
   href?: string;
   onClick?: MouseEventHandler<HTMLButtonElement>;
   title?: string;
+  withBetaBadge?: boolean;
 }
 
-export const HeaderLogo = ({ className, as = 'a', title = 'Altinn', href = '/' }: HeaderLogoProps) => {
+export const HeaderLogo = ({ className, as = 'a', title = 'Altinn', href = '/', withBetaBadge }: HeaderLogoProps) => {
   return (
     <ButtonBase as={as} className={cx(styles.logo, className)} href={href}>
       <DigdirLogomark className={styles.symbol} />
-      <span className={styles.text}>{title}</span>
+      <span className={styles.text}>{title}</span> {withBetaBadge && <Badge label="Beta" color="person" />}
     </ButtonBase>
   );
 };
