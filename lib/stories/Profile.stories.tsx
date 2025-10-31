22fe8f3debfe61917c88b68353308aaacea5efd8
Inge Fossland <inge@fosslandfoss.no>
2025-10-31 11:54:26 +0100

    fix: profile dashboard stories

--- lib/stories/Profile.stories.tsx
+++ lib/stories/Profile.stories.tsx
@@ -1,10 +1,19 @@
-import * as DashboardStories from "../components/Dashboard/Dashboard.stories";
+import { BellIcon, CogIcon, HeartIcon } from "@navikt/aksel-icons";
 import * as SettingsStories from "../components/Settings/SettingsList.stories";
 import * as ActivityLogStories from "../components/ActivityLog/ActivityLog.stories";
 import * as AccountListStories from "../components/Account/AccountList.stories";
 
-import { Heading, Layout, PageBase, Breadcrumbs } from "../components";
-import { useProfile } from "../../examples";
+import {
+  Heading,
+  Layout,
+  PageBase,
+  SettingsList,
+  Breadcrumbs,
+  DashboardHeader,
+  type DashboardHeaderProps,
+  type SettingsItemProps,
+} from "../components";
+import { useProfile, useAccounts, useSettings } from "../../examples";
 
 const meta = {
   title: "Demo/Profile",
@@ -19,9 +28,71 @@ export default meta;
 
 export const DashboardPage = () => {
   const { layout } = useProfile({});
+  const { defaultAccount } = useAccounts({});
+  const { items } = useSettings({});
+
+  const linkSettings = [
+    {
+      icon: HeartIcon,
+      title: "Mine aktører",
+      description: "Administrer aktører, favoritter og grupper.",
+      href: "/iframe.html?id=demo-profile--accounts-page",
+      groupId: "1",
+      badge: { label: "20 aktører" },
+    },
+    {
+      icon: BellIcon,
+      title: "Mine varslinger",
+      description: "Varslinger og varslingsprofiler.",
+      href: "/iframe.html?id=demo-profile--alerts-page",
+      groupId: "1",
+      badge: { label: "3 profiler" },
+    },
+    {
+      icon: CogIcon,
+      title: "Flere innstillinger",
+      href: "/iframe.html?id=demo-profile--settings-page",
+      as: "a",
+    },
+  ].map((item) => ({
+    ...item,
+    variant: "link",
+    as: "a",
+    groupId: "1",
+    linkIcon: true,
+  }));
+
+  const contactSettings =
+    items
+      ?.filter((item) => item.groupId === "contact")
+      ?.map((item) => ({
+        ...item,
+        groupId: "0",
+      })) || [];
+
+  const dashboardItems = [...contactSettings, ...linkSettings]?.map(
+    (item, index) => {
+      return {
+        ...item,
+        id: "s" + index,
+      };
+    }
+  );
+
+  const groups = {
+    "1": { title: "Min profil" },
+    "0": { title: "" },
+  };
+
   return (
     <Layout {...layout}>
-      <DashboardStories.UserDashboard />
+      <PageBase>
+        <DashboardHeader {...(defaultAccount as DashboardHeaderProps)} />
+        <SettingsList
+          groups={groups}
+          items={dashboardItems as SettingsItemProps[]}
+        />
+      </PageBase>
     </Layout>
   );
 };
