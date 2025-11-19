2344ded32e6b33317ce3fe9fa60fd6c59f1ab6de
Mateusz Bacherycz <mbacherycz@icloud.com>
2025-11-19 11:03:18 +0100

    feat: Make a GlobalMeny design reference story

---
+++ examples/layout/globalMenuDesignReference.tsx
@@ -0,0 +1,109 @@
+import {
+  Buildings2Icon,
+  ChatExclamationmarkIcon,
+  GlobeIcon,
+  InboxFillIcon,
+  InformationSquareIcon,
+  MenuGridIcon,
+  PadlockLockedFillIcon,
+  PersonCircleIcon,
+} from '@navikt/aksel-icons';
+import type { AccountMenuProps, GlobalMenuProps_old, MenuProps } from '../../lib';
+import { Badge } from '../../lib';
+import { accountMenu } from '../';
+
+const designReferenceMenuItems: MenuProps['items'] = [
+  {
+    id: 'inbox',
+    groupId: 'apps',
+    size: 'lg',
+    icon: InboxFillIcon,
+    title: (
+      <>
+        Innboks <Badge label="Beta" />
+      </>
+    ),
+    selected: true,
+  },
+  {
+    id: 'access-control',
+    groupId: 'apps',
+    size: 'lg',
+    icon: PadlockLockedFillIcon,
+    title: 'Tilgangsstyring',
+  },
+  {
+    id: 'forms',
+    groupId: 'apps',
+    size: 'lg',
+    icon: MenuGridIcon,
+    title: 'Alle skjema og tjenester',
+  },
+  {
+    id: 'about',
+    groupId: 'help',
+    size: 'sm',
+    icon: InformationSquareIcon,
+    title: 'Om nye altinn',
+  },
+  {
+    id: 'startup',
+    groupId: 'help',
+    size: 'sm',
+    icon: Buildings2Icon,
+    title: 'Starte og drive bedrift',
+  },
+  {
+    id: 'help',
+    groupId: 'help',
+    size: 'sm',
+    icon: ChatExclamationmarkIcon,
+    title: 'Trenger du hjelp?',
+  },
+  {
+    id: 'profile',
+    groupId: 'profile',
+    size: 'sm',
+    icon: PersonCircleIcon,
+    title: 'Din profil',
+  },
+  {
+    id: 'language',
+    groupId: 'profile',
+    size: 'sm',
+    icon: GlobeIcon,
+    title: 'Språk/language',
+  },
+];
+
+const designReferenceMenu: MenuProps = {
+  defaultIconTheme: 'surface',
+  groups: {
+    apps: {
+      divider: true,
+    },
+    help: {
+      defaultIconTheme: 'transparent',
+      divider: true,
+    },
+    profile: {
+      title: 'Logget inn som: Akvarell Søvnig',
+      defaultIconTheme: 'transparent',
+      defaultItemColor: 'person',
+    },
+  },
+  items: designReferenceMenuItems,
+};
+
+export const globalMenuDesignReference: GlobalMenuProps_old = {
+  accountMenu: {
+    ...(accountMenu as AccountMenuProps),
+    isVirtualized: true,
+  },
+  menu: designReferenceMenu,
+  currentEndUserLabel: 'Logget inn som: Akvarell Søvnig',
+  menuLabel: 'Meny',
+  backLabel: 'Tilbake',
+  logoutButton: { label: 'Logg ut' },
+  changeLabel: 'Endre',
+};
