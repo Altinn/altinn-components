0e2381c61620d81f5e058316d42f50e5629500c3
allinox <arnex04@gmail.com>
2025-12-02 19:36:19 +0100

    fix(UserListItem): Text-overflow to ellipsis on description

--- lib/components/UserListItem/UserListItem.tsx
+++ lib/components/UserListItem/UserListItem.tsx
@@ -1,43 +1,43 @@
-import { TokenIcon } from '@navikt/aksel-icons';
-import type { AvatarGroupProps, AvatarProps } from '../Avatar';
-import { Badge } from '../Badge';
-import type { IconProps, SvgElement } from '../Icon';
-import { ListItem, ListItemLabel } from '../List';
-import type { ListItemProps } from '../List';
+import { TokenIcon } from "@navikt/aksel-icons";
+import type { AvatarGroupProps, AvatarProps } from "../Avatar";
+import { Badge } from "../Badge";
+import type { IconProps, SvgElement } from "../Icon";
+import { ListItem, ListItemLabel } from "../List";
+import type { ListItemProps } from "../List";
 
-import styles from './userListItem.module.css';
+import styles from "./userListItem.module.css";
 
 export interface UserListItemProps
   extends Pick<
     ListItemProps,
-    | 'size'
-    | 'controls'
-    | 'onClick'
-    | 'loading'
-    | 'shadow'
-    | 'border'
-    | 'color'
-    | 'linkIcon'
-    | 'expanded'
-    | 'collapsible'
-    | 'children'
-    | 'as'
-    | 'interactive'
-    | 'id'
-    | 'className'
+    | "size"
+    | "controls"
+    | "onClick"
+    | "loading"
+    | "shadow"
+    | "border"
+    | "color"
+    | "linkIcon"
+    | "expanded"
+    | "collapsible"
+    | "children"
+    | "as"
+    | "interactive"
+    | "id"
+    | "className"
   > {
   /** Unique identifier for the user item */
   id: string;
   /** Name of the user */
   name: string;
   /** Type of user */
-  type: 'person' | 'company' | 'system';
+  type: "person" | "company" | "system";
   /** Description details for the user */
   description?: string;
   /** Roles of the user to be displayed */
   roleNames?: string[];
   /** The explicit heading or tag of the title, if diverting from the default heading */
-  titleAs?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p' | 'div' | 'span';
+  titleAs?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p" | "div" | "span";
   /** Display as subUnit */
   subUnit?: boolean;
 }
@@ -47,7 +47,7 @@ export const UserListItem = ({
   type,
   description,
   roleNames,
-  titleAs = 'h3',
+  titleAs = "h3",
   subUnit = false,
   loading = false,
   ...props
@@ -55,17 +55,17 @@ export const UserListItem = ({
   let icon: IconProps | SvgElement | AvatarProps | AvatarGroupProps;
 
   switch (type) {
-    case 'person':
-      icon = { name: name, type: 'person' };
+    case "person":
+      icon = { name: name, type: "person" };
       break;
-    case 'company':
-      icon = { name: name, type: 'company', isParent: !subUnit };
+    case "company":
+      icon = { name: name, type: "company", isParent: !subUnit };
       break;
-    case 'system':
+    case "system":
       icon = TokenIcon;
       break;
     default:
-      icon = { name: name, type: 'person' };
+      icon = { name: name, type: "person" };
   }
 
   const badges =
@@ -91,5 +91,14 @@ export const UserListItem = ({
     </div>
   );
 
-  return <ListItem icon={icon} ariaLabel={name} label={label} description={description} loading={loading} {...props} />;
+  return (
+    <ListItem
+      icon={icon}
+      ariaLabel={name}
+      label={label}
+      description={description}
+      loading={loading}
+      {...props}
+    />
+  );
 };
