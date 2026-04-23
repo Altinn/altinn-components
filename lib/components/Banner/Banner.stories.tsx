fe9b7785f051df24d354c95c239d8052d543b72d
Inge Fossland <inge@fosslandfoss.no>
2026-04-23 15:08:29 +0200

    feat: banner title + body and variants (#1142)

--- lib/components/Banner/Banner.stories.tsx
+++ lib/components/Banner/Banner.stories.tsx
@@ -1,6 +1,13 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
 import { Flex } from '../Page';
-import { Banner, type BannerColor, BannerColorEnum, type BannerProps } from './Banner';
+import {
+  Banner,
+  type BannerColor,
+  BannerColorEnum,
+  type BannerProps,
+  type BannerVariant,
+  BannerVariantEnum,
+} from './Banner';
 
 const meta = {
   title: 'Banner/Banner',
@@ -11,8 +18,11 @@ const meta = {
     sticky: true,
     closeTitle: 'Lukk',
     onClose: () => console.log('Banner closed'),
-    text: `Du ser nå på en beta-versjon av nye Altinn Innboks i et testmiljø. Alt innhold du ser her er basert på
-          testdata og kun ment for demonstrasjon.`,
+    title: (
+      <>
+        19. juni skrus gamle Altinn av. <a href="https://altinn.no">Dette må du passe på</a>.
+      </>
+    ),
   },
 } satisfies Meta<typeof Banner>;
 
@@ -23,22 +33,111 @@ export const Default: Story = {
   args: {},
 };
 
-export const WithoutCloseButton = () => {
-  return (
-    <Flex direction="col" spacing={3}>
-      <Banner text="Dette banneret har ingen lukke-knapp" />
-    </Flex>
-  );
+export const WarningBanner: Story = {
+  args: {
+    color: 'warning',
+    variant: 'alert',
+    title: (
+      <>
+        19. juni skrus gamle Altinn av. <a href="https://altinn.no">Dette må du passe på</a>.
+      </>
+    ),
+  },
+};
+
+export const WarningExpanded: Story = {
+  args: {
+    color: 'warning',
+    variant: 'alert',
+    title: '19. juni skrus gamle Altinn av.',
+    body: (
+      <>
+        <ul>
+          <li> 31. mai slettes påbegynte skjemaer fra utkast i gammel innboks.</li>
+          <li>19. juni skrus gamle Altinn av og du må begynne å ta i bruk ny innboks.</li>
+        </ul>
+
+        <p>
+          <a href="https://altinn.no">Mer informasjon.</a>
+        </p>
+      </>
+    ),
+  },
+};
+
+export const InfoBanner: Story = {
+  args: {
+    color: 'info',
+    variant: 'info',
+    title: (
+      <>
+        19. juni skrus gamle Altinn av. <a href="https://altinn.no">Dette må du passe på</a>.
+      </>
+    ),
+  },
+};
+
+export const InfoExpanded: Story = {
+  args: {
+    color: 'info',
+    variant: 'info',
+    title: '19. juni skrus gamle Altinn av.',
+    body: (
+      <>
+        <ul>
+          <li> 31. mai slettes påbegynte skjemaer fra utkast i gammel innboks.</li>
+          <li>19. juni skrus gamle Altinn av og du må begynne å ta i bruk ny innboks.</li>
+        </ul>
+
+        <p>
+          <a href="https://altinn.no">Mer informasjon.</a>
+        </p>
+      </>
+    ),
+  },
+};
+
+export const ErrorBanner: Story = {
+  args: {
+    color: 'danger',
+    variant: 'error',
+    title: (
+      <>
+        19. juni skrus gamle Altinn av. <a href="https://altinn.no">Dette må du passe på</a>.
+      </>
+    ),
+  },
+};
+
+export const ErrorExpanded: Story = {
+  args: {
+    color: 'danger',
+    variant: 'error',
+    title: '19. juni skrus gamle Altinn av.',
+    body: (
+      <>
+        <ul>
+          <li> 31. mai slettes påbegynte skjemaer fra utkast i gammel innboks.</li>
+          <li>19. juni skrus gamle Altinn av og du må begynne å ta i bruk ny innboks.</li>
+        </ul>
+
+        <p>
+          <a href="https://altinn.no">Mer informasjon.</a>
+        </p>
+      </>
+    ),
+  },
 };
 
 export const Colors = (args: BannerProps) => {
   const colors = Object.keys(BannerColorEnum) as BannerColor[];
+  const variants = Object.keys(BannerVariantEnum) as BannerVariant[];
 
   return (
     <Flex direction="col" spacing={3}>
-      {colors.map((color) => (
-        <Banner {...args} color={color} key={color} />
-      ))}
+      {colors.map((color) => {
+        return variants.map((variant) => <Banner {...args} color={color} variant={variant} key={variant} />);
+      })}
     </Flex>
   );
 };
